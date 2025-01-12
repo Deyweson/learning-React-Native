8485c7e581b49361c313b2b53880ca640591020f
Deyweson <deywesonalm@gmail.com>
2025-01-12 19:14:44 -0300

    :sparkles: Módulo 1: Conceitos básicos e simples sobre componentes, componentes de texto, botão, input, imagem

--- App.tsx
+++ App.tsx
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { Button, Image, SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+
+interface props {
+  name: string
+}
+
+const Greeting = ({ name }: props) => {
+  return (
+    <View>
+      <Text style={{ fontSize: 26 }} >Seja bem-vido, {name}</Text>
+    </View>
+  )
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Greeting name='Deyweson' />
+      <Image
+        source={{ uri: "https://avatars.githubusercontent.com/u/117497966?v=4" }}
+        style={{ height: 200, width: 200, top: 50 }}
+      />
+      <Button title='Curtir' />
+
+      <TouchableOpacity style={{ top: 50, padding: 10, backgroundColor: 'green', borderRadius:10 }}>
+        <Text style={{ color: '#fff' }}>Avançar</Text>
+      </TouchableOpacity>
+
+      <TextInput 
+        keyboardType='number-pad'
+        placeholder='Digite aqui...'
+        style={{top: 80, borderWidth: 1, borderColor: 'green', width: 100, textAlign: 'center'}}
+      />
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    top: 100
   },
 });
